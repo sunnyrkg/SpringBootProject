9fd92a689df484a6a08f7137faa3efffc7bf4d9c
risabh.gupta <risabh.gupta@ziplsy848.zycus.net>
2018-09-24 16:56:50 +0530

    Change

---
+++ src/main/java/com/zycus/boot/Launcher.java
@@ -0,0 +1,30 @@
+package com.zycus.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootApplication
+@ComponentScan(basePackages={
+		"com.zycus.boot.controllers",
+		"com.zycus.boot.services"
+})
+@EnableJpaRepositories(basePackages="com.zycus.boot.repositories")
+@EntityScan(basePackages="com.zycus.boot.entities")
+@EnableCaching
+public class Launcher {
+	public static void main(String...arg)
+	{
+		SpringApplication.run(Launcher.class, arg);
+	}
+	@Bean
+	public ObjectMapper mapper() {
+	  return new ObjectMapper();
+	}
+}
+
